4dded6f79b8c80582f69e0c886adae90730f266e
Fulvio <fulviocanducci@hotmail.com>
2019-10-05 23:05:00 -0300

    second

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import socketClient from 'socket.io-client'
 const socket = new socketClient('http://localhost:3001');
 
@@ -6,6 +6,7 @@ export function App() {
 
   const [message, setMessage] = useState('');
   const [list, setList] = useState([]);
+  const inputMessage = useRef();
 
   function handlerMessageChange(event) {
     const { value } = event.target;
@@ -14,25 +15,43 @@ export function App() {
 
   function handlerButtonSend() {
     socket.emit('chat message', message);
+    setMessage(''); 
+    handleInputMessageFocus();   
   }
 
+  function handleInputMessageFocus() {
+    if (inputMessage) {
+      inputMessage.current.focus();
+    }
+  }
+  
   useEffect(() => {
-    socket.on('chat message', (message) => {      
-      setList([...list, message]);
-      });
+    handleInputMessageFocus(); 
+    socket.emit('chat message', '');    
+  },[]);
+
+  useEffect(() => {
+    socket.on('chat message', (message) => {       
+      const messages = JSON.parse(message);     
+      setList(messages);      
+    });
   },[list]);
+
+  function ListShow({item}) {
+    return ( <li>{item}</li>);
+  }
 
   return (
     <>
       <div>
-        <input type="text" name="message" id="message" value={message} onChange={handlerMessageChange} />
+        <input type="text" ref={inputMessage} name="message" id="message" value={message} onChange={handlerMessageChange} />
       </div>
       <div>
         <button id="buttonSend" onClick={handlerButtonSend}>Send</button>
       </div>
       <div>
         <ul>
-          {list.map((item, index) => (<li key={index}>{item}</li>))}
+          {list.map((item, index) => (<ListShow key={index} item={item} />))}
         </ul>
       </div>
     </>
