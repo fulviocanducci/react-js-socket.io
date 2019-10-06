9e58c01034761edd9cf23d0c9ffdde28e15b2ccd
Fulvio <fulviocanducci@hotmail.com>
2019-10-05 21:59:53 -0300

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import socketClient from 'socket.io-client'
+const socket = new socketClient('http://localhost:3001');
 
-function App() {
+export function App() {
+
+  const [message, setMessage] = useState('');
+  const [list, setList] = useState([]);
+
+  function handlerMessageChange(event) {
+    const { value } = event.target;
+    setMessage(value);
+  }
+
+  function handlerButtonSend() {
+    socket.emit('chat message', message);
+  }
+
+  useEffect(() => {
+    socket.on('chat message', (message) => {      
+      setList([...list, message]);
+      });
+  },[list]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <input type="text" name="message" id="message" value={message} onChange={handlerMessageChange} />
+      </div>
+      <div>
+        <button id="buttonSend" onClick={handlerButtonSend}>Send</button>
+      </div>
+      <div>
+        <ul>
+          {list.map((item, index) => (<li key={index}>{item}</li>))}
+        </ul>
+      </div>
+    </>
   );
 }
 
